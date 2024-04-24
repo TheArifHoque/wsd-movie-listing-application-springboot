eac79b31b169364aa01ca251d5978675f220e2ba
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-04-24 23:56:08 +0600

    user service created

---
+++ src/main/java/com/arifhoque/wsdproject/service/UserService.java
@@ -0,0 +1,33 @@
+package com.arifhoque.wsdproject.service;
+
+import com.arifhoque.wsdproject.model.Movie;
+import com.arifhoque.wsdproject.model.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final List<User> users;
+
+    public UserService(List<User> users) {
+        this.users = users;
+    }
+
+    public User registerUser(User user) {
+        users.add(user);
+        return user;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void addMoviesToFavourites(String email, Movie movie) {
+            User user = users.stream().filter(u -> u.getEmail().equals(email)).findFirst().orElse(null);
+            if (user != null) {
+                user.getFavoriteMovies().add(movie);
+            } else
+                throw new RuntimeException("User not found");
+    }
+}
