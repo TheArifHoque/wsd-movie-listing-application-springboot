2d4ad42ae8fd26d2aa6f8cb547722ad2d875a642
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-04-24 22:37:57 +0600

    initial commit

---
+++ src/test/java/com/arifhoque/wsdproject/WsdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arifhoque.wsdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
