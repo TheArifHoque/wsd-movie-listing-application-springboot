291c1684a5e99a769df4b3a039460052c58ed5a3
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-04-25 09:31:50 +0600

    java doc added

--- src/main/java/com/arifhoque/wsdproject/model/User.java
+++ src/main/java/com/arifhoque/wsdproject/model/User.java
@@ -6,6 +6,11 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+/**
+ * This is a user model class. A user will have these behaviors
+ *
+ * @author Ariful Hoque
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
