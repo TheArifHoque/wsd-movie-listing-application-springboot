9ebf4a33a9326bdc55ba3c8e037ca6a56272e13d
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-04-25 09:31:36 +0600

    new methods added

--- src/main/java/com/arifhoque/wsdproject/service/MovieService.java
+++ src/main/java/com/arifhoque/wsdproject/service/MovieService.java
@@ -4,30 +4,44 @@ import com.arifhoque.wsdproject.model.Movie;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * This is movieService class where all the business logic for movies are written
+ *
+ * @author Ariful Hoque
+ */
 @Service
 public class MovieService {
-    private final List<Movie> movies;
+    private final List<Movie> movies = new ArrayList<>();
 
-    public MovieService(List<Movie> movies) {
-        this.movies = movies;
+    public MovieService() {
+        initializeMovies();
     }
 
     public void addMovie(Movie movie) {
         movies.add(movie);
     }
 
-    public void removeMovie(Movie movie) {
-        movies.remove(movie);
+    public void removeMovie(Integer id) {
+        movies.removeIf(movie -> movie.getId().equals(id));
     }
 
-    public List<Movie> getMovies() {
+    public List<Movie> getAllMovies() {
         List<Movie> result = new ArrayList<>(movies);
         result.sort(Comparator.comparing(Movie::getTitle));
         return result;
+    }
+
+    public Movie getMovieById(Integer id) {
+        for (Movie movie : movies) {
+            if (movie.getId().equals(id)) {
+                return movie;
+            }
+        }
+        return null;
     }
 
     public List<Movie> searchMovies(String query) {
@@ -42,5 +56,18 @@ public class MovieService {
             }
         }
         return results;
+    }
+
+    public void initializeMovies() {
+        Movie movie1 = new Movie(1, "The Shawshank Redemption", Arrays.asList("Tim Robbins", "Morgan Freeman"), "Drama", "1994-10-14", 25000000);
+        Movie movie2 = new Movie(2, "The Godfather", Arrays.asList("Marlon Brando", "Al Pacino"), "Crime", "1972-03-24", 6000000);
+        Movie movie3 = new Movie(5, "The Dark Knight", Arrays.asList("Christian Bale", "Heath Ledger"), "Action", "2008-07-18", 185000000);
+        Movie movie4 = new Movie(4, "Pulp Fiction", Arrays.asList("John Travolta", "Uma Thurman"), "Crime", "1994-10-14", 8000000);
+        Movie movie5 = new Movie(3, "Forrest Gump", Arrays.asList("Tom Hanks", "Robin Wright"), "Drama", "1994-07-06", 55000000);
+        movies.add(movie1);
+        movies.add(movie2);
+        movies.add(movie3);
+        movies.add(movie4);
+        movies.add(movie5);
     }
 }
