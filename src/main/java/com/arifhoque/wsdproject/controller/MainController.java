fb7ddd926e233dfa0a4ff03f5858cf458eb1c631
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-04-25 23:58:30 +0600

    every service is configured with it's endpoint

--- src/main/java/com/arifhoque/wsdproject/controller/MainController.java
+++ src/main/java/com/arifhoque/wsdproject/controller/MainController.java
@@ -30,28 +30,124 @@ public class MainController {
         this.movieService = movieService;
     }
 
+    /**
+     * Register user in system
+     *
+     * @param user information is provided
+     * @return created status will be given
+     */
     @PostMapping("/users/register")
     public ResponseEntity<?> registerUser(@RequestBody User user) {
         userService.registerUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    /**
+     * Get all user endpoint
+     *
+     * @return list of users
+     */
+    @GetMapping("/users/all")
+    public ResponseEntity<List<User>> getUser() {
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    /**
+     * Get individual user details by their unique email
+     *
+     * @param email to identify the user
+     * @return will return a user if found else not found status will be given
+     */
+    @GetMapping("/users/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email) {
+        if (userService.getUserByEmail(email) != null) {
+            return new ResponseEntity<>(userService.getUserByEmail(email), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * user can add movies to their favorites list
+     *
+     * @param email   to identify user
+     * @param movieId to add existing movie
+     * @return if operation is successful it'll return OK, if movie is not found it'll return no content and if user is not found it'll return not found
+     */
+    @PostMapping("/users/{email}/add-fav-movie")
+    public ResponseEntity<?> addFavMovie(@PathVariable String email, @RequestParam Integer movieId) {
+        if (userService.getUserByEmail(email) != null) {
+            Movie movie = movieService.getMovieById(movieId);
+            if (movie != null) {
+                userService.addMoviesToFavourites(email, movieId);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * user can remove movies to their favorites list
+     *
+     * @param email   to identify user
+     * @param movieId to add existing movie
+     * @return if operation is successful it'll return OK, if movie is not found it'll return no content and if user is not found it'll return not found
+     */
+    @PostMapping("/users/{email}/rem-fav-movie")
+    public ResponseEntity<?> removeFavMovie(@PathVariable String email, @RequestParam Integer movieId) {
+        if (userService.getUserByEmail(email) != null) {
+            Movie movie = movieService.getMovieById(movieId);
+            if (movie != null) {
+                userService.removeMoviesFromFavourites(email, movieId);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * List of favorite movie(s) for individual user
+     *
+     * @param email to identify user
+     * @return list of movie(s)
+     */
+    @GetMapping("/users/movies/{email}")
+    public ResponseEntity<List<Movie>> getFavMovieByEmail(@PathVariable String email) {
+        if (userService.getUserByEmail(email) != null) {
+            return new ResponseEntity<>(userService.getFavouriteMovies(email), HttpStatus.FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Endpoint to add new movie
+     *
+     * @param movie attributes
+     * @return created status
+     */
     @PostMapping("/movies/add")
     public ResponseEntity<?> addMovie(@RequestBody Movie movie) {
         movieService.addMovie(movie);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/users/all")
-    public ResponseEntity<List<User>> getUser() {
-        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
-    }
-
+    /**
+     * Get all movies
+     *
+     * @return List of movies
+     */
     @GetMapping("/movies/all")
     public ResponseEntity<List<Movie>> getMovies() {
         return new ResponseEntity<>(movieService.getAllMovies(), HttpStatus.OK);
     }
 
+    /**
+     * Search movie by its title/cast/category
+     *
+     * @param query for title/cast/category
+     * @return list of movies if available
+     */
     @GetMapping("/movies/search")
     public ResponseEntity<List<Movie>> searchMovies(@RequestParam("query") String query) {
         if (query == null || query.isEmpty()) {
@@ -62,10 +158,16 @@ public class MainController {
         return new ResponseEntity<>(movieService.searchMovies(query), HttpStatus.OK);
     }
 
+    /**
+     * User can search movie by its movieId
+     *
+     * @param movieId to get Movie
+     * @return movie if found else return not found status
+     */
     @GetMapping("/movies/{id}")
-    public ResponseEntity<Movie> getMovieById(@PathVariable("id") Integer id) {
-        if (movieService.getMovieById(id) != null) {
-            return new ResponseEntity<>(movieService.getMovieById(id), HttpStatus.OK);
+    public ResponseEntity<Movie> getMovieById(@PathVariable("id") Integer movieId) {
+        if (movieService.getMovieById(movieId) != null) {
+            return new ResponseEntity<>(movieService.getMovieById(movieId), HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
