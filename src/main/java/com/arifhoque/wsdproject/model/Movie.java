846b86c54eb7db9b70ce07dab98243f9991ce0db
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2024-04-25 09:31:21 +0600

    movieId added

--- src/main/java/com/arifhoque/wsdproject/model/Movie.java
+++ src/main/java/com/arifhoque/wsdproject/model/Movie.java
@@ -6,13 +6,19 @@ import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+/**
+ * This is a movie model class. A movie will have these attributes
+ *
+ * @author Ariful Hoque
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Movie {
+    private Integer id;
     private String title;
     private List<String> cast;
     private String category;
     private String releaseDate;
-    private double budget;
+    private float budget;
 }
